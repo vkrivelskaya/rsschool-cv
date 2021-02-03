8549fadcd0df678461c3e1ef824f81ade4dd2e7a
Valentina Krivelskaya <v.krivelskaya@mail.ru>
2021-02-03 23:24:24 +0300

    feat: code refactoring

--- src/main.js
+++ src/main.js
@@ -7,23 +7,6 @@ const classes = {
     PLAYING: 'playing',
 }
 
-function onKeyDown(e) {  
-    playSound(e.keyCode);
-}
-
-function onClick(e) {
-    let target = e.target;
-    let key;
-    while (true) {
-        key = target.getAttribute('data-key');
-        if (key || target === this) {
-            break;
-        }
-        target = target.parentElement;
-    }
-    playSound(key);
-}
-
 function playSound(keyCode) {
     const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
     const key = document.querySelector(`div[data-key="${keyCode}"]`);
@@ -34,22 +17,41 @@ function playSound(keyCode) {
     audio.play();
 }
 
+function onKeyDown(e) { 
+    playSound(e.keyCode);
+}
+
+function getKeyFromElement(element, lastParent) {    
+    let key;
+    while (true) {
+        key = element.getAttribute('data-key');
+        if (key || element === lastParent) {
+            break;
+        }
+        element = element.parentElement;
+    }
+    return key;
+}
+
+function onClick(e) { 
+    const key = getKeyFromElement(e.target, this);   
+    if (key) {
+        playSound(key);
+    }    
+}
+
 function removeClassPlaying(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove(classes.PLAYING);
 }
 
-function init() {
-    const keys = Array.from(document.querySelectorAll(selectors.KEY));
+function init() {    
     const buttons = document.querySelector(selectors.BUTTONS);
-
-    keys.forEach(key => key.addEventListener('transitionend', removeClassPlaying));
+    
+    buttons.addEventListener('transitionend', removeClassPlaying);    
     window.addEventListener('keydown', onKeyDown);
     buttons.addEventListener('click', onClick);
 }
 
 init();
-
-
-
   
