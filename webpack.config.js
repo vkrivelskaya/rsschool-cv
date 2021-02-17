3fb39126a913add55eaaa2d4e2784f67e6252bc3
Valentina Krivelskaya <v.krivelskaya@mail.ru>
2021-02-17 22:50:17 +0300

    feat: add styles to app

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,64 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev;
+const optimization = () => {
+    const config = {
+      splitChunks: {
+        chunks: 'all'
+      }
+    }
+  
+    if (isProd) {
+      config.minimizer = [
+        new OptimizeCssAssetWebpackPlugin(),
+        new TerserWebpackPlugin()
+      ]
+    }
+  
+    return config
+  }
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     mode: 'development',
-    entry: './src/index.js',
+    entry: './index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: optimization(),
+    devServer: {
+        port: 4200,
+        hot: isDev
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] 
+            }
+        ]
     }
 }
